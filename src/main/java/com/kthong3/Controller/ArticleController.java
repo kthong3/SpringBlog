908886b5c67e58b8187719f9a5e26cb44eb748ec
kthong3 <kthongsakounh@gmail.com>
2018-10-03 15:07:01 -0500

    add Spring autowiring and mapping for Articles index

--- src/main/java/com/kthong3/Controller/ArticleController.java
+++ src/main/java/com/kthong3/Controller/ArticleController.java
@@ -2,15 +2,21 @@ package com.kthong3.Controller;
 
 import com.kthong3.Entity.Article;
 import com.kthong3.Service.ArticleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
 
 @RestController
+@RequestMapping("/articles")
 public class ArticleController {
 
+    @Autowired
     private ArticleService articleService;
 
+    @RequestMapping(method = RequestMethod.GET)
     public Collection<Article> getAllArticles(){
         return articleService.getAllArticles();
     }
--- src/main/java/com/kthong3/Service/ArticleService.java
+++ src/main/java/com/kthong3/Service/ArticleService.java
@@ -2,6 +2,7 @@ package com.kthong3.Service;
 
 import com.kthong3.Dao.ArticleDao;
 import com.kthong3.Entity.Article;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
@@ -9,6 +10,7 @@ import java.util.Collection;
 @Service
 public class ArticleService {
 
+    @Autowired
     private ArticleDao articleDao;
 
     public Collection<Article> getAllArticles(){
